830765ad4198ea1793c39cd4a1abefdba962b3db
MelikaNobakhtian <haminob48@gmail.com>
2020-02-11 11:14:15 +0330

    first step

---
+++ Template/T1.Tests/GradedTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestCommon;
+
+namespace T1.Tests
+{
+    [DeploymentItem("TestData")]
+    [TestClass()]
+    public class GradedTests
+    {
+        [TestMethod(), Timeout(100)]
+        public void SolveTest_Q0Problem()
+        {
+            RunTest(new Q0Problem("TD1"));            
+        }
+
+        public static void RunTest(Processor p)
+        {
+            TestTools.RunLocalTest("T1", p.Process, p.TestDataName, p.Verifier, VerifyResultWithoutOrder: p.VerifyResultWithoutOrder,
+                excludedTestCases: p.ExcludedTestCases);
+        }        
+    }
+}
---
+++ Template/T1/Q0Problem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TestCommon;
+
+namespace T1
+{
+    public class Q0Problem: Processor
+    {
+        public Q0Problem(string testDataName) : base(testDataName)
+        {
+        }
+
+        public override string Process(string inStr)
+        {
+            return Solve(int.Parse(inStr)).ToString();
+        }
+
+        public int Solve(int n) => n+1;        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
