1c10426bbe018e220e0167bb31b80a6923996085
MelikaNobakhtian <haminob48@gmail.com>
2020-02-12 09:41:14 +0330

    Assignment 1

---
+++ A1/A1/Q1MazeExit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A1
+{
+    public class Q1MazeExit : Processor
+    {
+        public Q1MazeExit(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long, long, long>)Solve);
+
+        public long Solve(long nodeCount, long[][] edges, long StartNode, long EndNode)
+        {
+            List<List<long>> graph = new List<List<long>>();
+            for (int i = 0; i < nodeCount; i++)
+            {
+                graph.Add(new List<long>());
+            }
+            for (int i = 0; i < edges.Length; i++)
+            {
+                graph[(int)edges[i][0] - 1].Add(edges[i][1] - 1);
+                graph[(int)edges[i][1] - 1].Add(edges[i][0] - 1);
+            }
+
+            return DFS(StartNode - 1, EndNode - 1, graph, nodeCount);
+        }
+
+        long DFS(long start, long end, List<List<long>> graph, long n)
+        {
+            bool[] visit = new bool[n];
+            return Explore(start, end, graph, visit);
+        }
+
+        private long Explore(long start, long end, List<List<long>> graph, bool[] visit)
+        {
+            if (start == end)
+                return 1;
+            visit[start] = true;
+            foreach (var v in graph[(int)start])
+            {
+                long result = 0;
+                if (!visit[v])
+                    result = Explore(v, end, graph, visit);
+                if (result == 1) return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ A1/A1/Q3Acyclic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A1
+{
+    public class Q3Acyclic : Processor
+    {
+        public Q3Acyclic(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long>)Solve);
+
+        public long Solve(long nodeCount, long[][] edges)
+        {
+            List<List<long>> graph = new List<List<long>>();
+            for (int i = 0; i < nodeCount; i++)
+            {
+                graph.Add(new List<long>());
+            }
+            for (int i = 0; i < edges.Length; i++)
+            {
+                graph[(int)edges[i][0] - 1].Add(edges[i][1] - 1);
+            }
+
+            return HasCycle(graph, nodeCount);
+        }
+
+        public long HasCycle(List<List<long>> graph, long n)
+        {
+            bool[] visit = new bool[n];
+            bool[] inStack = new bool[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (DFs(i, graph, visit, inStack))
+                    return 1;
+            }
+            return 0;
+        }
+
+        private bool DFs(int i, List<List<long>> graph, bool[] visit, bool[] inStack)
+        {
+            if (!visit[i])
+            {
+                visit[i] = true;
+                inStack[i] = true;
+                foreach (var v in graph[i])
+                {
+                    if (!visit[v] && DFs((int)v, graph, visit, inStack))
+                        return true;
+                    else if (inStack[v])
+                        return true;
+                }
+            }
+            inStack[i] = false;
+            return false;
+        }
+    }
+}
