280d0a7ae44c92a77cc9179c9a649fdd43c20b94
MelikaNobakhtian <haminob48@gmail.com>
2020-02-29 22:56:52 +0330

    hard work but all tests didnt pass

---
+++ A3/A3/Q1MinCost.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A3
+{
+    public class Q1MinCost : Processor
+    {
+        public Q1MinCost(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long, long, long>)Solve);
+
+
+        public long Solve(long nodeCount, long[][] edges, long startNode, long endNode)
+        {
+            var result = Dijkstra(edges, startNode - 1, endNode - 1, nodeCount);
+            return result[endNode - 1]!=long.MaxValue ? result[endNode-1] : -1;
+        }
+
+        public long[] Dijkstra(long[][] edges, long start, long end, long nodecount)
+        {
+            long[] dist = new long[nodecount];
+            List<long> idx = new List<long>();
+            List<long> priorityqueue = new List<long>();
+            for (int i = 0; i < nodecount; i++)
+            {
+                priorityqueue.Add(long.MaxValue);
+                idx.Add(i);
+                dist[i] = long.MaxValue;
+            }
+            dist[start] = 0;
+            priorityqueue[0] = 0;
+            idx[0] = start;
+            idx[(int)start] = 0;
+            while (idx.Count != 0)
+            {
+                var v = idx[0];
+                if (v == end) return dist;
+                var index = idx.IndexOf(v);
+                idx.Remove(v);
+                priorityqueue.RemoveAt(index);
+                BuildHeap(priorityqueue, idx);
+                for (int i = 0; i < edges.Length; i++)
+                {
+                    if (edges[i][0] - 1 == v)
+                    {
+                        var u = edges[i][1] - 1;
+                        var newdist = dist[v] + edges[i][2];
+                        if (edges[i][1] - 1 != start && newdist < dist[u] && newdist > 0)
+                        {
+                            dist[u] = newdist;
+                            var j = idx.IndexOf(u);
+                            ChangePriority(j, newdist, priorityqueue, idx);
+                            if (u == end && !idx.Contains(u)) return dist;
+                        }
+                    }
+
+
+                }
+                BuildHeap(priorityqueue, idx);
+            }
+
+            return dist;
+        }
+
+        public void ChangePriority(int i, long p, List<long> heap, List<long> idx)
+        {
+            var oldvalue = heap[i];
+            heap[i] = p;
+            if (p > oldvalue)
+                SiftDown(i, heap, idx);
+            else
+                SiftUp(i, heap, idx);
+        }
+
+        public void SiftUp(int i, List<long> heap, List<long> idx)
+        {
+            while (i > 0 && heap[(i - 1) / 2] > heap[i])
+            {
+                (heap[i], heap[(i - 1) / 2]) = (heap[(i - 1) / 2], heap[i]);
+                (idx[i], idx[(i - 1) / 2]) = (idx[(i - 1) / 2], idx[i]);
+            }
+        }
+
+        public void BuildHeap(List<long> array, List<long> idx)
+        {
+            int last = array.Count / 2;
+            for (int i = last; i >= 0; i--)
+                SiftDown(i, array, idx);
+        }
+
+        public void SiftDown(int i, List<long> heap, List<long> idx)
+        {
+            int minindex = i;
+            int leftchild;
+            int rightchild;
+            if (2 * i + 1 < heap.Count)
+            {
+                leftchild = 2 * i + 1;
+                if (heap[leftchild] < heap[minindex])
+                    minindex = leftchild;
+            }
+            if (2 * i + 2 < heap.Count)
+            {
+                rightchild = 2 * i + 2;
+                if (heap[minindex] > heap[rightchild])
+                    minindex = rightchild;
+            }
+            if (i != minindex)
+            {
+                (heap[i], heap[minindex]) = (heap[minindex], heap[i]);
+                (idx[i], idx[minindex]) = (idx[minindex], idx[i]);
+                SiftDown(minindex, heap, idx);
+            }
+
+        }
+    }
+}
---
+++ A3/A3/Q3ExchangingMoney.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+namespace A3
+{
+    public class Q3ExchangingMoney : Processor
+    {
+        public Q3ExchangingMoney(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long, string[]>)Solve);
+
+        long[] dist;
+        string[] pre;
+        bool[] infinitDis;
+        string[] path;
+
+        public string[] Solve(long nodeCount, long[][] edges, long startNode)
+        {
+            dist = new long[nodeCount];
+            pre = new string[nodeCount];
+            infinitDis = new bool[nodeCount];
+            path = new string[nodeCount];
+            for (int i = 0; i < nodeCount; i++)
+            {
+                dist[i] = long.MaxValue;
+            }
+            BellmanFord(nodeCount, edges, startNode - 1);
+            for (int i = 0; i < nodeCount; i++)
+            {
+                if (infinitDis[i] == true)
+                    path[i] = "-";
+                else if (pre[i] == null && dist[i] == long.MaxValue)
+                    path[i] = "*";
+                else
+                    path[i] = dist[i].ToString();
+            }
+            return path;
+        }
+
+        public void BellmanFord(long nodeCount, long[][] edges, long start)
+        {
+            dist[start] = 0;
+            for (int i = 1; i < nodeCount; i++)
+            {
+                foreach (var edge in edges)
+                    Relax(edge, dist);
+            }
+            foreach (var edge in edges)
+            {
+                var result = Relax(edge, dist);
+                if (result && !infinitDis[edge[1] - 1])
+                {
+                    InfiniteArbitrage(edges, nodeCount, edge[1] - 1);
+                }
+            }
+        }
+
+        private void InfiniteArbitrage(long[][] edges, long NodeCount, long StartNode)
+        {
+            bool[] process = new bool[NodeCount];
+            process[StartNode] = true;
+            infinitDis[StartNode] = true;
+            Queue<long> vertices = new Queue<long>();
+            vertices.Enqueue(StartNode);
+            while (vertices.Count != 0)
+            {
+                var u = vertices.Dequeue();
+                for (int i = 0; i < edges.Length; i++)
+                    if (edges[i][0] - 1 == u)
+                    {
+                        var v = edges[i][1] - 1;
+                        if (!infinitDis[v])
+                            infinitDis[v] = true;
+                        if (process[v] == false)
+                        {
+                            vertices.Enqueue(v);
+                            process[v] = true;
+                        }
+                    }
+            }
+        }
+
+        private bool Relax(long[] edge, long[] distance)
+        {
+            var v = edge[1] - 1;
+            var u = edge[0] - 1;
+            if (distance[u] == long.MaxValue)
+                return false;
+            var newdist = distance[u] + edge[2];
+            if (distance[v] > newdist)
+            {
+                distance[v] = newdist;
+                pre[v] = u.ToString();
+                return true;
+            }
+            return false;
+        }
+    }
+}
