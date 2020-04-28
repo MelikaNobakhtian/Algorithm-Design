1dc5af187421e7aaed89c6f01a39c94b82aa590d
MelikaNobakhtian <haminob48@gmail.com>
2020-04-28 11:35:14 +0430

    hopeless

---
+++ MeasureIt.src/UnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace PerformanceMeasurement
+{
+    [TestFixture]
+    // I know these aren't unit tests :) 
+    internal class UnitTests
+    {
+        [TestCase]
+        public void TestMeasureActionDoesNotCrash()
+        {
+            var stats = LinqPadUX.ComputerSpecs;
+            var timeIt = LinqPadUX.Measure.Action(() => "Hello".GetHashCode());
+        }
+        [TestCase]
+        public void TestDefaultBenchmarkToWebBrowserDoesNotCrash()
+        {
+            LinqPadUX.DefaultBenchmarkToWebBrowser();
+        }
+    }
+}
