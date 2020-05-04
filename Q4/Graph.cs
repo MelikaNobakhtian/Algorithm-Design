4c118361d1e11f171a31ea0b87df3e7c40eac10b
MelikaNobakhtian <haminob48@gmail.com>
2020-05-04 19:28:35 +0430

    Q4

---
+++ Q4/Graph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q4
+{
+    public class Graph
+    {
+        public List<Node> Nodes { get; set; }
+        public Graph()
+        {
+            Nodes = new List<Node>();
+        }
+
+        public void MakeSubGraph(Func<int,int,bool> func,int start,int end)
+        {
+            for(int i = start; i < end; i++)
+            {
+                Nodes.Add(new Node(i));
+            }
+            for(int i = start; i < end; i++)
+            {
+                for(int j = i + 1; j < end; j++)
+                {
+                    if (func(i, j))
+                    {
+                        Nodes[i].Edges.Add(j);
+                        Nodes[j].Edges.Add(i);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Q4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Func<int, int, bool>[] funcs = new Func<int, int, bool>[3];
+            funcs[0] = (s, t) => true;
+            funcs[1] = (s, t) => (s % 2 == 0 && t % 2 == 0) || (s % 2 == 1 && t % 2 == 1);
+            funcs[2] = (s, t) => (Math.Abs(s - t) % 2 == 1);
+            int number_of_subgraphs = 3;
+            Graph network = new Graph();
+            for(int i = 0; i < number_of_subgraphs; i++)
+            {
+                int nodes = rnd.Next(3, 8);
+                int start = network.Nodes.Count;
+                int end = start + nodes;
+                int methodnumber = rnd.Next(0, 2);
+                network.MakeSubGraph(funcs[methodnumber], start, end);
+            }
+        }
+    }
+}
